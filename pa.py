e685e24e8dfe140cb65f86165e3182aeb1d7568b
Tigas Weeknd <tiagocastroifpi@gmail.com>
2024-08-09 14:22:50 -0300

    pa.py

--- pa.py
+++ pa.py
@@ -3,7 +3,7 @@ numa = int(input("Digite um número: "))
 numb = int(input("Digite outro número: "))
 soma = 0
 if numa < numb:
-    for x in range(numa, numb):
+    for x in range(numa, numb + 1):
          soma += x
     print(soma)
 else:
