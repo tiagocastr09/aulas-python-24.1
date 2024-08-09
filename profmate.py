d565b34476240c0ee4f68d2a2ec3aeeb10bc6478
Tigas Weeknd <tiagocastroifpi@gmail.com>
2024-08-09 14:36:36 -0300

    profmate.py

--- profmate.py
+++ profmate.py
@@ -1,8 +1,8 @@
 #Questão 2 da aula 9
-a = int(input("Digite o primeiro termo: "))
-b = int (input("Digite a quantidade de termos: "))
+pt = int(input("Digite o primeiro termo: "))
+qt = int (input("Digite a quantidade de termos: "))
 r = int(input("Digite a razão: "))
-s = 0
-for x in range(a, b +1, r):
-    s += x
-print(s)
+ut = pt +(qt - 1) * r
+for termo in range(pt, ut +1, r):
+    print(termo)
+print("Fim")
