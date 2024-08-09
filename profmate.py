f73aec7d7631160a047a8ff017f9991b14015df9
Tigas Weeknd <tiagocastroifpi@gmail.com>
2024-08-09 14:00:20 -0300

    Delete profmate.py

--- profmate.py
+++
@@ -1,8 +0,0 @@
-#Questão 2 da aula 9
-a = int(input("Digite o primeiro termo: "))
-b = int (input("Digite a quantidade de termos: "))
-r = int(input("Digite a razão: "))
-s = 0
-for x in range(a, b +1, r):
-    s += x
-print(s)
